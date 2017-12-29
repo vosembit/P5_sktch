d862980d13a3c0c623bdc23151520e9ac0cd1cc6
daladno13 <slickindub@gmail.com>
2017-12-29 13:51:17 +0300

    Update green.js

--- sketches/green/green.js
+++ sketches/green/green.js
@@ -1,7 +1,6 @@
 var x = 0;
 var y = 0;
 var t1 = 0;
-var t2 = 0;
 var c;
 
 function setup() {
@@ -10,29 +9,16 @@ function setup() {
     cnv.parent('sketch-holder');
     colorMode(HSB, 255);
     centerCanvas();
-    c = color(32, 255, 255);
-    background(c);
 }
 
 function draw() {
-    //    if (millis() - t1 > 500) {
-    //        c = color(random(255), 255, random(255));
-    //        background(c);
-    //        t1 = millis();
-    //    }
-    //    if (millis() - t2 > 500) {
-    //        c = color(random(255), 255, 255);
-    //        background(c);
-    //        t2 = millis();
-    //    }
-    if (millis() - t2 > 500) {
-        c = random(255);
-        for (var i = 0; i < 255; i++) {
-            var x = color(c, 255, i);
-            background(x);
-        }
-        t2 = millis();
+
+    if (millis() - t1 > 500) {
+        c = color(random(255), 255, 255);
+        background(c);
     }
+    t1 = millis();
+
 }
 
 function centerCanvas() {
